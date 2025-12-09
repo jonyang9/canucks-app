dcc41afc9df790776edc1fd57ff46a20f6d02af7
Jonathan Yang <yangjonathan9@gmail.com>
2025-12-08 16:56:45 -0800

    add validation helper functions and data structures

--- backend/src/scraper_helpers.py
+++ backend/src/scraper_helpers.py
@@ -1,9 +1,78 @@
 # Helpers for scraper.py
 
+# Useful data structures
+# Note: - Arizona Coyotes transfered to the Utah HC/Mammoth
+#       - Both Mammoth and Utah HC refer to the code 'UTA'
+team_codes_dict = {
+    'Ducks': 'ANA',
+    'Coyotes': 'ARI',
+    'Bruins': 'BOS',
+    'Sabres': 'BUF',
+    'Flames': 'CGY',
+    'Hurricanes': 'CAR',
+    'Blackhawks': 'CHI',
+    'Avalanche': 'COL',
+    'Blue Jackets': 'CBJ',
+    'Stars': 'DAL',
+    'Red Wings': 'DET',
+    'Oilers': 'EDM',
+    'Panthers': 'FLA',
+    'Kings': 'LAK',
+    'Wild': 'MIN',
+    'Canadiens': 'MTL',
+    'Predators': 'NSH',
+    'Devils': 'NJD',
+    'Islanders': 'NYI',
+    'Rangers': 'NYR',
+    'Senators': 'OTT',
+    'Flyers': 'PHI',
+    'Penguins': 'PIT',
+    'Sharks': 'SJS',
+    'Kraken': 'SEA',
+    'Blues': 'STL',
+    'Lightning': 'TBL',
+    'Maple Leafs': 'TOR',
+    'Canucks': 'VAN',
+    'Golden Knights': 'VGK',
+    'Capitals': 'WSH',
+    'Jets': 'WPG',
+    'Mammoth': 'UTA',
+    'Utah HC': 'UTA'
+}
+
+team_codes = ['ANA', 'ARI', 'BOS', 'BUF', 'CGY', 'CAR', 'CHI', 'COL', 'CBJ', 'DAL', 'DET', 'EDM', 'FLA', 'LAK', 'MIN', 'MTL', 'NSH', 'NJD', 'NYI', 'NYR', 'OTT', 'PHI', 'PIT', 'SJS', 'SEA', 'STL', 'TBL', 'TOR', 'VAN', 'VGK', 'WSH', 'WPG', 'UTA']
+
+
+# def parse_game_date(game_str)
+
+def validate_season(season):
+    # Effect: raises an error if season isn't a string with format: '20252026'
+    if not isinstance(season, str):
+        raise TypeError('Season must be a string')
+    
+    if len(season) != 8 or not season.isdigit():
+        raise ValueError('Season is not the right format')
+    
+    first_year = int(season[:4])
+    second_year = int(season[4:])
+    if second_year - first_year != 1:
+        raise ValueError('Season is not the right format')
+    
+def validate_team_code(team_code):
+    # Effect: raises an error if team_code isn't a string and a valid NHL team code
+    if not isinstance(team_code, str):
+        raise TypeError('Team code must be a string')
+    
+    if team_code not in team_codes:
+        raise ValueError("Team code is not a valid NHL team code")
+
+    
+
+
 def canucks_win(game_str):
-    # Example of str: '2024-10-09 - Flames 6, Canucks 5',
-    # returns true if the Canucks won
-    # used to determine outcome of a game for supervised learning label
+    # Example of str: '2024-10-09 - Flames 6, Canucks 5'
+    # returns 1 if the Canucks won
+    # used to determine outcome of a game as the supervised learning label
     
     score_str = game_str.split(' - ')[1].strip()
     team_scores = score_str.split(', ')
@@ -20,3 +89,11 @@ def canucks_win(game_str):
     opp_goals = int(opp_goals)
 
     return 1 if canucks_goals > opp_goals else 0
+
+def build_url(season, team1_code, team2_code='All'):
+    validate_season(season)
+    validate_team_code(team1_code)
+    if team2_code != 'All':
+        validate_team_code(team2_code)
+    
+    return f'https://www.naturalstattrick.com/games.php?fromseason={season}&thru_season={season}&stype=2&sit=5v5&loc=B&team={team1_code}&team2={team2_code}&rate=y'
