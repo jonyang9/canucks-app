f2c1dad60033a3381f444e3287090dd5a6d0053b
Jonathan Yang <yangjonathan9@gmail.com>
2025-12-11 17:55:56 -0800

    add fetch data helper function

--- backend/src/scraper.py
+++ backend/src/scraper.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from scraper_helpers import canucks_win, home_game, build_url
+from scraper_helpers import canucks_win, home_game, fetch_table
+from pathlib import Path
 
 # This script fetches historical game data from https://www.naturalstattrick.com then processes it
 # The site only lets you view 3 seasons at once, to simplify we can just look at 5 seasons individually to build our dataset
@@ -10,24 +11,25 @@ season = '20252026'
 team1_code = 'VAN'
 team2_code = None
 
-# read_html returns a list of DataFrames; get the first (and only) one
-df = pd.read_html(build_url(season, team1_code), attrs={'id': 'teams'})[0] 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+df_raw = fetch_table(season, team1_code, team2_code)
+print(df_raw)
+
+df_eng = pd.DataFrame()
 
 # Add win/loss supervised learning label
-df['Win'] = df['Game'].map(canucks_win, na_action='ignore')
+df_eng['Win'] = df_raw['Game'].map(canucks_win, na_action='ignore')
 
 # Each column in Game column looks like: '2024-10-09 - Flames 6, Canucks 5'
 # Subtracts consecutive datetimes creating a timedelta column to calculate the days between games feature
-df['Game Date'] = pd.to_datetime(df['Game'].map(lambda x: x.split(' - ')[0]))
-df['Days Since Last Game'] = (df['Game Date'] - df['Game Date'].shift(1)).dt.days
+game_datetimes = pd.to_datetime(df_raw['Game'].map(lambda x: x.split(' - ')[0]))
+df_eng['Days Since Last Game'] = (game_datetimes - game_datetimes.shift(1)).dt.days
 
 # Add home game feature
-df['Home'] = df['Game'].map(home_game, na_action='ignore')
+df_eng['Home'] = df_raw['Game'].map(home_game, na_action='ignore')
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-print(df.dtypes)
-print(df)
-df.to_csv(f"../data/{team1_code}_{season}.csv", index=False)
+print(df_eng)
 
-df.drop(columns=['Unnamed: 2', 'Team', 'Game',], inplace=True)
+
